ea5da44209457ced6f8a96fc3d7e3ca56cbffb6d
RNH Creative <rnhcreativeteam@gmail.com>
2025-10-13 09:44:05 +0700

    fix: ability to choose existing price unit name
    Signed-off-by: RNH Creative <rnhcreativeteam@gmail.com>

--- app/Filament/Tenant/Resources/ProductResource/RelationManagers/PriceUnitsRelationManager.php
+++ app/Filament/Tenant/Resources/ProductResource/RelationManagers/PriceUnitsRelationManager.php
@@ -2,30 +2,75 @@
 
 namespace App\Filament\Tenant\Resources\ProductResource\RelationManagers;
 
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Support\RawJs;
+use Laravel\Pennant\Feature;
 use App\Features\ProductStock;
 use App\Models\Tenants\Profile;
 use App\Models\Tenants\Setting;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Support\RawJs;
-use Filament\Tables;
-use Filament\Tables\Table;
+use App\Models\Tenants\PriceUnit;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Select;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Pennant\Feature;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class PriceUnitsRelationManager extends RelationManager
 {
     protected static string $relationship = 'priceUnits';
 
+    public array $unitOptions = [];
+
+    public function mount(): void
+    {
+        $unitOptions = PriceUnit::pluck('unit', 'unit')->toArray();
+
+        // make units is uppercase and only show unique units
+        $unitOptions = array_map(function ($unit) {
+            return strtoupper($unit);
+        }, $unitOptions);
+
+        // Only show unique units
+        $unitOptions = array_combine($unitOptions, $unitOptions);
+
+        foreach ($unitOptions as $key => $value) {
+            $this->unitOptions[$value] = $value;
+        }
+    }
+
     public function form(Form $form): Form
     {
+        $self = $this;
+
         return $form
             ->columns(1)
             ->schema([
-                Forms\Components\TextInput::make('unit')
+                Select::make('unit')
+                    ->label(__('Unit Name'))
                     ->translateLabel()
-                    ->placeholder(__('Meter(m), Box or what you want'))
+                    ->options($self->unitOptions)
+                    ->createOptionForm([
+                        Forms\Components\TextInput::make('label')
+                            ->label(__('Unit Name'))
+                            ->translateLabel()
+                            ->required(),
+                    ])
+                    ->createOptionUsing(function (array $data) {
+                        // tambahkan opsi ke array lokal
+                        $this->unitOptions[$data['label']] = $data['label'];
+
+                        // kembalikan nilai yang akan dipilih otomatis setelah create
+                        return $data['label'];
+                    })
+                    ->createOptionAction(fn (Forms\Components\Actions\Action $action)
+                        => $action
+                            ->modalWidth('md')
+                    )
+                    ->searchable()
+                    ->reactive()
                     ->required(),
                 Forms\Components\TextInput::make('selling_price')
                     ->translateLabel()
@@ -43,6 +88,7 @@ class PriceUnitsRelationManager extends RelationManager
             ->recordTitleAttribute('selling_price')
             ->columns([
                 Tables\Columns\TextColumn::make('unit')
+                    ->label(__('Unit Name'))
                     ->translateLabel(),
                 Tables\Columns\TextColumn::make('selling_price')
                     ->translateLabel()
