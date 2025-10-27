853aaecdcb5832d173c9e20844ef1c698a737079
RNH Creative <rnhcreativeteam@gmail.com>
2025-10-27 15:18:13 +0700

    fix: cannot upload product when app is running via cloudflare tunneling
    Signed-off-by: RNH Creative <rnhcreativeteam@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -67,9 +67,9 @@ class AppServiceProvider extends ServiceProvider
         Feature::resolveScopeUsing(fn ($driver) => null);
         Feature::discover();
 
-        // if (config('app.env') === 'production') {
-        //     URL::forceScheme('https');
-        // }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
         $url = request()->getSchemeAndHttpHost();
         config(['app.url' => $url]);
